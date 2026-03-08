5fb881e72f8ba27c0e75216db261941129dfa313
wmentrekin <wmentrekin@gmail.com>
2026-03-08 03:01:51 -0400

    fix data clear task and checkpoint successful full pipeline run

--- sql/0002_clear_bronze_silver_data.sql
+++ sql/0002_clear_bronze_silver_data.sql
@@ -3,14 +3,14 @@
 
 begin;
 
-truncate table silver.event_asset_lineage;
-truncate table silver.assets;
-truncate table silver.events;
-
-truncate table bronze.raw_event_asset_links;
-truncate table bronze.raw_assets;
-truncate table bronze.raw_events;
-truncate table bronze.ingest_runs;
+truncate table
+  silver.event_asset_lineage,
+  silver.assets,
+  silver.events,
+  bronze.raw_event_asset_links,
+  bronze.raw_assets,
+  bronze.raw_events,
+  bronze.ingest_runs
+restart identity;
 
 commit;
-
